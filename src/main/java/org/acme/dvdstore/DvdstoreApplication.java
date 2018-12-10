2b2b007edea97e10a41fdc1f8054b3518bb584ab
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-12-10 13:03:14 +0200

    Initial commit of project structure.

---
+++ src/main/java/org/acme/dvdstore/DvdstoreApplication.java
@@ -0,0 +1,12 @@
+package org.acme.dvdstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdstoreApplication.class, args);
+	}
+}
---
+++ src/test/java/org/acme/dvdstore/DvdstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package org.acme.dvdstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DvdstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
